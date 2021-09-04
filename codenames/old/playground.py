ce5c7720f8d44cca1da317795db40e9aad77c250
Asaf Kali <asaf.kali@mail.huji.ac.il>
2021-09-05 02:41:54 +0300

    Add online game manager to playground, minor fixes to SNA player.
    Former-commit-id: afb341db2822dfd16e3a639a6e0a3f058901fbf0

--- codenames/old/playground.py
+++ codenames/old/playground.py
@@ -1,19 +1,15 @@
 # type: ignore
-import numpy as np
-
-from codenames.model_loader import load_language
-from codenames.utils import configure_logging
-from codenames.visualizer import pretty_print_similarities
-
-configure_logging()
-LANGUAGE_FOLDER = "language_data"
-ENGLISH_DATA_FILE = "english.bin"
 
 # %%
+
+from codenames.model_loader import load_language
 
 w = load_language("english")
 
 # %%
+import numpy as np
+from codenames.visualizer import pretty_print_similarities
+
 v1 = w.get_vector("park")
 v2 = w.get_vector("beach")
 a = w.most_similar((v1 / np.linalg.norm(v1) + v2 / np.linalg.norm(v2)), topn=50)
@@ -21,10 +17,36 @@ pretty_print_similarities(a)
 
 # %%
 from codenames.game.base import TeamColor
-from codenames.game.builder import build_simple_state
+from codenames.game.builder import words_to_random_board
+from codenames.game.manager import GameManager
 from codenames.solvers.sna_solvers.sna_hinter import SnaHinter
+from codenames.online.online_game_manager import NamecodingGameManager
+from codenames.online.online_adapter import NamecodingLanguage
+from codenames.solvers.cli_players import CliGuesser
+import logging
 
-board_words = [
+from codenames.utils import configure_logging
+
+configure_logging()
+
+log = logging.getLogger(__name__)
+blue_hinter = SnaHinter("Leonardo", team_color=TeamColor.BLUE)
+blue_guesser = CliGuesser("Bard", team_color=TeamColor.BLUE)
+red_hinter = SnaHinter("Adam", team_color=TeamColor.RED)
+red_guesser = CliGuesser("Eve", team_color=TeamColor.RED)
+
+# %%
+game_manager = NamecodingGameManager(blue_hinter, red_hinter, blue_guesser, red_guesser)
+try:
+    game_manager.auto_start(language=NamecodingLanguage.ENGLISH, clock=False)
+except Exception as e:
+    log.exception("Error occurred")
+    raise e
+finally:
+    game_manager.close()
+# %%
+
+words = [
     "cloak",
     "kiss",
     "flood",
@@ -50,9 +72,15 @@ board_words = [
     "earth",
     "park",
     "gymnast",
-]  # ['king', 'queen', 'teenage', 'tomato', 'parrot', 'london', 'spiderman']
+    "king",
+    "queen",
+    "teenage",
+    "tomato",
+    "parrot",
+    "london",
+    "spiderman",
+]
 
-game_state = build_simple_state(words=board_words)
-hinter = SnaHinter(name="Michael", team_color=TeamColor.RED)
-hinter.notify_game_starts(language="english", board=game_state.board)
-print(f"Hinter guessed: {hinter.pick_hint(state=game_state)}")
+board = words_to_random_board(words=words)
+game_manager = GameManager(blue_hinter, red_hinter, blue_guesser, red_guesser)
+game_manager.run_game(language="english", board=board)
