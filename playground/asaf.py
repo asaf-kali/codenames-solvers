47fb5f39611a83db6799931857f4a0e1265f63a6
Asaf Kali <asaf.kali@mail.huji.ac.il>
2021-09-10 03:39:46 +0300

    Playground reorg
    Former-commit-id: c9ab0608fd849e70c2d2d75927911382d8af7a62

--- playground/asaf.py
+++ playground/asaf.py
@@ -1,15 +1,21 @@
-# type: ignore
-
 # %%
+import logging
 import os
+from time import sleep
 
 from codenames.game.base import TeamColor
 from codenames.game.builder import words_to_random_board
-from codenames.game.manager import GameManager
+from codenames.game.manager import GameManager, QuitGame
+from codenames.online.online_adapter import NamecodingLanguage
+from codenames.online.online_game_manager import NamecodingGameManager
+from codenames.solvers.naive.naive_guesser import NaiveGuesser
+from codenames.solvers.naive.naive_hinter import NaiveHinter
 from codenames.solvers.utils.model_loader import MODEL_NAME_ENV_KEY
 from codenames.utils import configure_logging
 
 configure_logging()
+
+log = logging.getLogger(__name__)
 os.environ[MODEL_NAME_ENV_KEY] = "wiki-50"
 
 words = [
@@ -49,13 +55,27 @@ words = [
 
 board = words_to_random_board(words=words, seed=3)
 
-# %% Run game - SNA
-from codenames.solvers.sna_solvers.sna_hinter import SnaHinter  # noqa: E402
-from codenames.solvers.naive.naive_guesser import NaiveGuesser  # noqa: E402
-
-blue_hinter = SnaHinter("Leonardo", team_color=TeamColor.BLUE)
+blue_hinter = NaiveHinter("Leonardo", team_color=TeamColor.BLUE)
 blue_guesser = NaiveGuesser("Bard", team_color=TeamColor.BLUE)
-red_hinter = SnaHinter("Adam", team_color=TeamColor.RED)
+red_hinter = NaiveHinter("Adam", team_color=TeamColor.RED)
 red_guesser = NaiveGuesser("Eve", team_color=TeamColor.RED)
+
+# %% Offline
 game_manager = GameManager(blue_hinter, red_hinter, blue_guesser, red_guesser)
 game_manager.run_game(language="english", board=board)
+
+# %% Online
+online_manager = None
+try:
+    online_manager = NamecodingGameManager(blue_hinter, red_hinter, blue_guesser, red_guesser)
+    online_manager.auto_start(language=NamecodingLanguage.ENGLISH, clock=False)
+    sleep(1)
+except QuitGame:
+    pass
+except Exception as e:  # noqa
+    log.exception("Error occurred")
+finally:
+    if online_manager is not None:
+        log.info(f"Winner: {online_manager.winner}")
+        online_manager.close()
+log.info("Done")
