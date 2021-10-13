0709bb069ee4a522d1b26fe557fb89fbde01e85e
Michael Kali <89668997+mkali-personal@users.noreply.github.com>
2021-10-13 19:35:20 +0300

    merge videos begining
    Former-commit-id: e46f36c3c7458f28ad7b3cf4c090793ecc6f01f7

--- videos/merge_videos.py
+++ videos/merge_videos.py
@@ -5,9 +5,13 @@ import subprocess
 manim_video = VideoFileClip("media/videos/explanation/1080p60/KalirmozExplanation.mp4")
 graphviz_video = VideoFileClip("videos/lynx_example.mp4")
 # %%
-clip_1 = manim_video.subclip(1,4)
+clip_1 = manim_video.subclip(0,1)
 clip_2 = manim_video.subclip(1,2)
-clip_3 = manim_video.subclip(4,6)
+clip_3 = manim_video.subclip(1,2)
+
+composited_clip = CompositeVideoClip([clip_1, clip_2, clip_3])
+
+composited_clip.write_videofile(f"composite_test.mp4", fps=clip_1.fps, remove_temp=True, codec="libx264") # , temp_audiofile="temp-audio.m4a" , audio_codec="aac"
 
 
 # %%
