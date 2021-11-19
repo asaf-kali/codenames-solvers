1ebdaf0fb2ebed4218072ed19a7e781b81db4487
Asaf Kali <asaf.kali@mail.huji.ac.il>
2021-11-19 16:18:09 +0200

    Fix random builder consistency.
    Former-commit-id: 8eaf9df1fd77e0748bfd1c798f6b256cf9ac3ab9

--- codenames/game/builder.py
+++ codenames/game/builder.py
@@ -1,29 +1,29 @@
 import random
-from typing import Iterable, Tuple
+from typing import Iterable, Tuple, Sequence
 
 from codenames.game.base import BLACK_AMOUNT, Board, Card, CardColor, HinterGameState
 
 
-def _extract_random_subset(complete_set: set, subset_size: int) -> Tuple[set, set]:
-    subset = set(random.sample(complete_set, subset_size))
-    reduced_set = complete_set.difference(subset)
-    return reduced_set, subset
+def _extract_random_subset(elements: Sequence, subset_size: int) -> Tuple[Sequence, Sequence]:
+    sample = random.sample(elements, k=subset_size)
+    remaining = tuple(e for e in elements if e not in sample)
+    return remaining, sample
 
 
 def words_to_random_board(words: Iterable[str], seed: int = None) -> Board:
-    random.seed(a=seed)
+    random.seed(seed)
 
-    words_set = set(words)
-    board_size = len(words_set)
+    words_list = tuple(words)
+    board_size = len(words_list)
     red_amount = board_size // 3
     blue_amount = red_amount + 1
     gray_amount = board_size - red_amount - blue_amount - BLACK_AMOUNT
 
-    words_set, red_words = _extract_random_subset(words_set, red_amount)
-    words_set, blue_words = _extract_random_subset(words_set, blue_amount)
-    words_set, gray_words = _extract_random_subset(words_set, gray_amount)
-    words_set, black_words = _extract_random_subset(words_set, BLACK_AMOUNT)
-    assert len(words_set) == 0
+    words_list, red_words = _extract_random_subset(words_list, red_amount)
+    words_list, blue_words = _extract_random_subset(words_list, blue_amount)
+    words_list, gray_words = _extract_random_subset(words_list, gray_amount)
+    words_list, black_words = _extract_random_subset(words_list, BLACK_AMOUNT)
+    assert len(words_list) == 0
 
     red_cards = [Card(word, CardColor.RED) for word in red_words]
     blue_cards = [Card(word, CardColor.BLUE) for word in blue_words]
