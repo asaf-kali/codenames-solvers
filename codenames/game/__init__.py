643a4581c18d27df7f7307e94db37746accbb133
Asaf Kali <akali93@gmail.com>
2022-01-03 22:33:30 +0200

    Add `ModelAdapter` concept.
    Former-commit-id: a949ed17f5dd61f177f06ece72b79780d0e3ecbe

--- codenames/game/__init__.py
+++ codenames/game/__init__.py
@@ -2,4 +2,5 @@ from .base import *  # noqa
 from .builder import *  # noqa
 from .exceptions import *  # noqa
 from .manager import *  # noqa
+from .model_format_adapter import *  # noqa
 from .player import *  # noqa
