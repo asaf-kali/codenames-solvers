9ddc1cc7aade3c8259def3418be0b476d6adb71e
Asaf Kali <asaf.kali@mail.huji.ac.il>
2022-01-07 16:48:10 +0200

    Minor bug fix in naive solver
    Former-commit-id: c21002023c1e03999622d9690aab97be7837b2a4

--- playground/online.py
+++ playground/online.py
@@ -4,14 +4,18 @@ from time import sleep
 
 from codenames.game import QuitGame
 from codenames.online import NamecodingGameManager, NamecodingLanguage
-from codenames.solvers.naive import NaiveGuesser, NaiveHinter  # type: ignore  # noqa
-from codenames.solvers.sna_solvers import SnaHinter  # type: ignore  # noqa
+from codenames.solvers import (  # type: ignore  # noqa
+    NaiveGuesser,
+    NaiveHinter,
+    SnaHinter,
+)
 from codenames.utils import configure_logging
 from language_data.model_loader import (
     IS_STEMMED_ENV_KEY,
     MODEL_NAME_ENV_KEY,
     load_language_async,
 )
+from playground.model_adapters import HEBREW_SUFFIX_ADAPTER
 
 configure_logging()
 log = logging.getLogger(__name__)
@@ -27,10 +31,12 @@ load_language_async(language="hebrew")  # type: ignore
 def online_game():
     online_manager = None
     try:
-        blue_hinter = NaiveHinter("Leonardo")
-        blue_guesser = NaiveGuesser("Bard")
-        red_hinter = SnaHinter("Adam")
-        red_guesser = NaiveGuesser("Eve")
+        adapter = HEBREW_SUFFIX_ADAPTER
+        # adapter = DEFAULT_MODEL_ADAPTER
+        blue_hinter = NaiveHinter("Leonardo", model_adapter=adapter)
+        blue_guesser = NaiveGuesser("Bard", model_adapter=adapter)
+        red_hinter = NaiveHinter("Adam", model_adapter=adapter)
+        red_guesser = NaiveGuesser("Eve", model_adapter=adapter)
         online_manager = NamecodingGameManager(blue_hinter, red_hinter, blue_guesser, red_guesser)
         online_manager.auto_start(language=NamecodingLanguage.HEBREW, clock=False)
         sleep(1)
