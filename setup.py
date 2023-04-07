b376c232a94fd8e7a9207a056cb067619c48a73f
Asaf Kali <asaf.kali@mail.huji.ac.il>
2023-04-07 16:22:09 +0300

    Infra work.

--- setup.py
+++ setup.py
@@ -1,5 +1,23 @@
 from setuptools import setup
 
+BASE_DEPENDENCIES = [
+    # Core
+    "the-spymaster-util>=2.0",
+    "codenames~=1.2",
+    "pydantic~=1.9",
+    "numpy~=1.21",
+    "gensim~=4.1",
+    "pandas~=1.3",
+    "scipy~=1.7",
+    "generic-iterative-stemmer~=0.2",
+    # Algo
+    "networkx~=2.6",
+    "python-louvain~=0.15",
+    "editdistance~=0.6",
+]
+GPT_DEPENDENCIES = ["openai~=0.27"]
+ALL_DEPENDENCIES = BASE_DEPENDENCIES + GPT_DEPENDENCIES
+
 setup(
     name="codenames-solvers",
     version="1.2.3",
@@ -7,20 +25,10 @@ setup(
     author="Asaf Kali",
     author_email="asaf.kali@mail.huji.ac.il",
     url="https://github.com/asaf-kali/codenames-solvers",
-    install_requires=[
-        # Core
-        "the-spymaster-util>=2.0",
-        "codenames~=1.1",
-        "pydantic~=1.9",
-        "numpy~=1.21",
-        "gensim~=4.1",
-        "pandas~=1.3",
-        "scipy~=1.7",
-        "generic-iterative-stemmer~=0.2",
-        # Algo
-        "networkx~=2.6",
-        "python-louvain~=0.15",
-        "editdistance~=0.6",
-    ],
+    install_requires=BASE_DEPENDENCIES,
+    extras_require={
+        "all": ALL_DEPENDENCIES,
+        "gpt": GPT_DEPENDENCIES,
+    },
     include_package_data=True,
 )
