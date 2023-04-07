288c27b7a68cf0e75090f875699b682e5c2e7ecf
Asaf Kali <asaf.kali@mail.huji.ac.il>
2023-04-07 16:22:22 +0300

    Add GPT basics.

--- playground/offline.py
+++ playground/offline.py
@@ -1,4 +1,5 @@
 import logging
+import os
 import random
 
 from codenames.game import GameRunner, QuitGame
@@ -7,6 +8,7 @@ from playground.boards.english import *  # noqa
 from playground.boards.hebrew import *  # noqa
 from playground.printer import print_results
 from solvers.cli_players import CliGuesser  # noqa
+from solvers.gpt.gpt_hinter import GPTHinter
 from solvers.models import (  # noqa
     DEFAULT_MODEL_ADAPTER,
     HEBREW_SUFFIX_ADAPTER,
@@ -24,30 +26,31 @@ random.seed(42)
 configure_logging(level="DEBUG", mute_solvers=False)
 log = logging.getLogger(__name__)
 
-# model_id = ModelIdentifier("english", "wiki-50", False)
+model_id = ModelIdentifier(language="english", model_name="wiki-50", is_stemmed=False)
 # model_id = ModelIdentifier("english", "google-300", False)
 # model_id = ModelIdentifier("hebrew", "twitter", False)
 # model_id = ModelIdentifier("hebrew", "ft-200", False)
-model_id = ModelIdentifier(language="hebrew", model_name="skv-ft-150", is_stemmed=True)
+# model_id = ModelIdentifier(language="hebrew", model_name="skv-ft-150", is_stemmed=True)
 # model_id = ModelIdentifier("hebrew", "skv-cbow-150", True)
 
 # os.environ[MODEL_NAME_ENV_KEY] = model_id.model_name
 # os.environ[IS_STEMMED_ENV_KEY] = "1" if model_id.is_stemmed else ""
 adapter = HEBREW_SUFFIX_ADAPTER if model_id.language == "hebrew" and model_id.is_stemmed else DEFAULT_MODEL_ADAPTER
 load_model_async(model_id)
+GPT_API_KEY = os.getenv("OPENAI_API_KEY")
 
 
-def run_offline(board: Board = HEBREW_BOARD_6):  # noqa: F405
+def run_offline(board: Board = ENGLISH_BOARD_1):  # noqa: F405
     log.info("Running offline game...")
     game_runner = None
     try:
         # blue_hinter = OlympicHinter("Einstein", model_adapter=adapter)
         # red_hinter = OlympicHinter("Yoda", model_adapter=adapter)
-        blue_hinter = NaiveHinter("Einstein", model_identifier=model_id, model_adapter=adapter)
-        red_hinter = NaiveHinter("Yoda", model_identifier=model_id, model_adapter=adapter)
+        blue_hinter = GPTHinter("Yoda", api_key=GPT_API_KEY)
+        red_hinter = NaiveHinter("Einstein", model_identifier=model_id, model_adapter=adapter, max_group_size=3)
         blue_guesser = NaiveGuesser(name="Newton", model_identifier=model_id, model_adapter=adapter)
-        # red_guesser = NaiveGuesser(name="Anakin", model_identifier=model_id, model_adapter=adapter)
-        red_guesser = CliGuesser(name="Anakin")
+        red_guesser = NaiveGuesser(name="Anakin", model_identifier=model_id, model_adapter=adapter)
+        # red_guesser = CliGuesser(name="Anakin")
         game_runner = GameRunner(blue_hinter, red_hinter, blue_guesser, red_guesser)
         game_runner.run_game(language=model_id.language, board=board)  # noqa
     except QuitGame:
--- playground/printer.py
+++ playground/printer.py
@@ -1,6 +1,8 @@
 import logging
+from collections import defaultdict
+from typing import List
 
-from codenames.game import GameRunner
+from codenames.game import GameRunner, GivenHint, GivenGuess
 
 log = logging.getLogger(__name__)
 
@@ -9,10 +11,17 @@ def print_results(game_runner: GameRunner):
     if game_runner is None:
         return
     log.info(f"\n{game_runner.state.board}")
-    hint_strings = [str(hint) for hint in game_runner.state.raw_hints]
-    hints_string = "\n".join(hint_strings)
-    log.info(f"\nHints:\n{hints_string}")
-    # log.info("Guesses:")
-    # for guess in game_runner.given_guesses:
-    #     log.info(guess)
+    guesses_by_hints = defaultdict(list)
+    for guess in game_runner.state.given_guesses:
+        guesses_by_hints[guess.given_hint].append(guess)
+    log.info("\nGame moves:")
+    for hint, guesses in guesses_by_hints.items():
+        hint: GivenHint
+        guesses: List[GivenGuess]
+        log.info(f"{hint.team_color} team turn, hinter said: {hint}")
+        for guess in guesses:
+            log.info(f"   Guesser said: {guess}")
+    red_score = len([card for card in game_runner.state.board.red_cards if card.revealed])
+    blue_score = len([card for card in game_runner.state.board.blue_cards if card.revealed])
+    log.info(f"\nScore: Blue {blue_score} - {red_score} Red")
     log.info(f"\nWinner: {game_runner.state.winner}")
