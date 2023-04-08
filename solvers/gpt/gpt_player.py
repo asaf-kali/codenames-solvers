02bcfbb3743dc439ad54e1aa7db8319c62432086
Asaf Kali <asaf.kali@mail.huji.ac.il>
2023-04-08 18:24:13 +0300

    GPT progress.

--- solvers/gpt/gpt_player.py
+++ solvers/gpt/gpt_player.py
@@ -1,9 +1,21 @@
+import json
 import logging
 import re
 from abc import ABC
 from typing import List, Optional
 
-from codenames.game import Player, Score, TeamColor
+from codenames.game.color import TeamColor
+from codenames.game.move import (
+    GivenGuess,
+    GivenHint,
+    GuessMove,
+    HintMove,
+    Move,
+    PassMove,
+)
+from codenames.game.player import Player
+from codenames.game.score import Score
+from codenames.game.state import BaseGameState
 from openai import ChatCompletion
 
 from solvers.gpt.instructions import load_instructions
@@ -41,6 +53,21 @@ class GPTPlayer(Player, ABC):
             f"Blue: {score.blue.revealed}/{score.blue.total}."
         )
 
+    @classmethod
+    def build_moves_repr(cls, state: BaseGameState) -> Optional[str]:
+        moves: List[Move] = state.moves
+        if not moves:
+            return None
+        moves_repr = []
+        for move in moves:
+            if isinstance(move, HintMove):
+                moves_repr.append(hint_repr(hint=move.given_hint))
+            elif isinstance(move, GuessMove):
+                moves_repr.append(guess_repr(guess=move.given_guess))
+            elif isinstance(move, PassMove):
+                moves_repr.append(pass_repr(move=move))
+        return "\n".join(moves_repr)
+
     def generate_completion(self, messages: List[dict]) -> dict:
         log.debug("Sending completion request", extra={"payload_size": len(str(messages)), "messages": messages})
         response = ChatCompletion.create(
@@ -49,8 +76,17 @@ class GPTPlayer(Player, ABC):
             api_key=self.api_key,
             temperature=self.temperature,
         )
-        log.debug("Got completion response", extra={"response": response})
+        usage = response.get("usage")
+        log.debug(f"Got completion response, usage: {usage}", extra={"response": response})
         return response
+
+
+def extract_data_from_response(completion_result: dict) -> dict:
+    response_content: str = completion_result["choices"][0]["message"]["content"]
+    data_raw = find_json_in_string(response_content)
+    log.debug(f"Parsing content: {data_raw}")
+    data = json.loads(data_raw)
+    return data
 
 
 def find_json_in_string(data: str) -> str:
@@ -58,3 +94,15 @@ def find_json_in_string(data: str) -> str:
     if match:
         return match.group(0)
     raise ValueError("No JSON found in string")
+
+
+def hint_repr(hint: GivenHint) -> str:
+    return f"{hint.team_color} hinter said: '{hint.word}', {hint.card_amount} cards."
+
+
+def guess_repr(guess: GivenGuess) -> str:
+    return f"{guess.team} guesser said: {guess}."
+
+
+def pass_repr(move: PassMove) -> str:
+    return f"{move.team_color} team guesser passed the turn."
