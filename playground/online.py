40b16e4efeba1b42ef99943649746f86c9a8347d
Asaf Kali <akali93@gmail.com>
2023-04-09 04:01:34 +0300

    Online fixes.

--- playground/online.py
+++ playground/online.py
@@ -1,12 +1,15 @@
 import logging
 import os
 
-from codenames.game import QuitGame
-from codenames.online import NamecodingGameRunner, NamecodingLanguage  # noqa
+from codenames.game.exceptions import QuitGame
+from codenames.online.online_adapter import NamecodingLanguage
+from codenames.online.online_game_runner import NamecodingGameRunner
 
+from playground.offline import GPT_API_KEY
 from playground.printer import print_results
-from solvers.cli_players import CliGuesser, CliHinter  # noqa
-from solvers.models import (  # noqa
+from solvers.gpt.gpt_guesser import GPTGuesser
+from solvers.gpt.gpt_hinter import GPTHinter
+from solvers.models import (
     DEFAULT_MODEL_ADAPTER,
     HEBREW_SUFFIX_ADAPTER,
     IS_STEMMED_ENV_KEY,
@@ -14,17 +17,16 @@ from solvers.models import (  # noqa
     ModelIdentifier,
     load_language_async,
 )
-from solvers.naive import NaiveGuesser, NaiveHinter  # noqa
-from solvers.olympic.olympic_hinter import OlympicHinter  # noqa
+from solvers.naive import NaiveGuesser, NaiveHinter
 from utils import configure_logging
 
 configure_logging(level="INFO", mute_solvers=False, mute_online=True)
 log = logging.getLogger(__name__)
 
-# model_id = ModelIdentifier("english", "wiki-50", False)
+model_id = ModelIdentifier(language="english", model_name="wiki-50", is_stemmed=False)
 # model_id = ModelIdentifier("english", "google-300", False)
 # model_id = ModelIdentifier("hebrew", "twitter", False)
-model_id = ModelIdentifier(language="hebrew", model_name="ft-200", is_stemmed=False)
+# model_id = ModelIdentifier(language="hebrew", model_name="ft-200", is_stemmed=False)
 # model_id = ModelIdentifier("hebrew", "skv-ft-150", True)
 # model_id = ModelIdentifier("hebrew", "skv-cbow-150", True)
 
@@ -40,13 +42,13 @@ def run_online():
     log.info("Running online game...")
     online_manager = None
     try:
-        # blue_hinter = OlympicHinter("Einstein", model_adapter=adapter)
-        # red_hinter = OlympicHinter("Yoda", model_adapter=adapter)
-        blue_hinter = NaiveHinter("Einstein", model_identifier=model_id, model_adapter=adapter)  # noqa
-        red_hinter = NaiveHinter("Yoda", model_identifier=model_id, model_adapter=adapter)  # noqa
+        blue_hinter = GPTHinter(name="Einstein", api_key=GPT_API_KEY)
+        # blue_hinter = NaiveHinter("Einstein", model_identifier=model_id, model_adapter=adapter)  # noqa
+        red_hinter = NaiveHinter(name="Yoda", model_identifier=model_id, model_adapter=adapter)  # noqa
         blue_guesser = NaiveGuesser(name="Newton", model_identifier=model_id, model_adapter=adapter)  # noqa
-        red_guesser = NaiveGuesser(name="Anakin", model_identifier=model_id, model_adapter=adapter)  # noqa
-        online_manager = NamecodingGameRunner(blue_hinter, red_hinter, None, None, show_host=False)
+        red_guesser = GPTGuesser(name="Anakin", api_key=GPT_API_KEY)
+        # red_guesser = NaiveGuesser(name="Anakin", model_identifier=model_id, model_adapter=adapter)  # noqa
+        online_manager = NamecodingGameRunner(blue_hinter, red_hinter, blue_guesser, red_guesser, show_host=True)
         # online_manager = NamecodingGameRunner(blue_hinter, red_hinter, blue_guesser, red_guesser, show_host=False)
         online_manager.auto_start(language=namecoding_language, clock=False)
     except QuitGame:
