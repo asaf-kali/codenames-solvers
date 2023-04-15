de25b12e8b63b3cd9741078f8891e545cef6fe3d
Asaf Kali <akali93@gmail.com>
2023-04-15 16:11:14 +0300

    Package data fix.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 BASE_DEPENDENCIES = [
     # Core
     "the-spymaster-util>=2.0",
-    "codenames~=2.0",
+    "codenames~=2.1",
     "pydantic~=1.9",
     "numpy~=1.21",
     "gensim~=4.1",
@@ -20,13 +20,14 @@ ALL_DEPENDENCIES = BASE_DEPENDENCIES + GPT_DEPENDENCIES
 
 setup(
     name="codenames-solvers",
-    version="1.4.2",
+    version="1.4.3",
     description="Codenames board game solvers implementation in python.",
     author="Asaf Kali",
     author_email="akali93@gmail.com",
     url="https://github.com/asaf-kali/codenames-solvers",
     install_requires=BASE_DEPENDENCIES,
     packages=find_packages(include=["solvers", "solvers.*"]),
+    package_data={"": ["*.json"]},
     extras_require={
         "all": ALL_DEPENDENCIES,
         "gpt": GPT_DEPENDENCIES,
