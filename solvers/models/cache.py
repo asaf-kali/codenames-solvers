a73207329e0d4bdbac020977779a7389b44380d8
Asaf Kali <asaf.kali@mail.huji.ac.il>
2023-12-30 02:25:36 +0200

    🔧 Model loader: restructure.

--- solvers/models/cache.py
+++ solvers/models/cache.py
@@ -1,27 +1,15 @@
 import logging
 import os
 from os.path import expanduser
-from threading import Lock, Thread
-from typing import Dict, Optional
+from threading import Lock
+from typing import Dict
 
 from generic_iterative_stemmer.models import StemmedKeyedVectors
 from gensim.models import KeyedVectors
-from pydantic import BaseModel
+
+from solvers.models.identifier import ModelIdentifier
 
 log = logging.getLogger(__name__)
-
-MODEL_NAME_ENV_KEY = "MODEL_NAME"
-IS_STEMMED_ENV_KEY = "IS_STEMMED"
-DEFAULT_MODEL_NAME = "wiki-50"
-
-
-class ModelIdentifier(BaseModel):
-    language: str
-    model_name: str
-    is_stemmed: bool = False
-
-    def __hash__(self):
-        return hash(f"{self.language}-{self.model_name}-{self.is_stemmed}")
 
 
 class ModelCache:
@@ -59,47 +47,6 @@ class ModelCache:
         )
         log.info("Model loaded", extra={"model": model_identifier.dict()})
         return model
-
-
-_model_cache = ModelCache()
-
-
-def set_language_data_folder(language_data_folder: str):
-    _model_cache.language_data_folder = language_data_folder
-
-
-def is_loaded(model_identifier: ModelIdentifier) -> bool:
-    return _model_cache.is_loaded(model_identifier)
-
-
-def load_model(model_identifier: ModelIdentifier) -> KeyedVectors:
-    return _model_cache.load_model(model_identifier)
-
-
-def load_model_async(model_identifier: ModelIdentifier):
-    thread = Thread(target=load_model, args=(model_identifier,))
-    thread.start()
-
-
-def load_language(language: str, model_name: Optional[str] = None, is_stemmed: Optional[bool] = None) -> KeyedVectors:
-    if model_name is None:
-        model_name = os.environ.get(key=MODEL_NAME_ENV_KEY, default=DEFAULT_MODEL_NAME)  # type: ignore
-    if is_stemmed is None:
-        is_stemmed = bool(os.environ.get(key=IS_STEMMED_ENV_KEY, default=False))  # type: ignore
-    model_identifier = ModelIdentifier(language=language, model_name=model_name, is_stemmed=is_stemmed)
-    return load_model(model_identifier)
-
-
-def load_language_async(language: str, model_name: Optional[str] = None, is_stemmed: Optional[bool] = None):
-    thread = Thread(target=load_language, args=(language, model_name, is_stemmed), daemon=True)
-    thread.start()
-
-
-# def load_word2vec_format(language_base_folder: str, model_name: str) -> KeyedVectors:
-#     # Where data structure is `*.bin`
-#     file_path = os.path.join(language_base_folder, f"{model_name}.bin")
-#     data = KeyedVectors.load_word2vec_format(file_path, binary=True)
-#     return data
 
 
 def load_kv_format(language_base_folder: str, model_name: str, is_stemmed: bool = False) -> KeyedVectors:
---
+++ solvers/models/loader.py
@@ -0,0 +1,55 @@
+import logging
+import os
+from threading import Thread
+from typing import Optional
+
+from gensim.models import KeyedVectors
+
+from solvers.models.cache import ModelCache
+from solvers.models.identifier import ModelIdentifier
+
+log = logging.getLogger(__name__)
+
+MODEL_NAME_ENV_KEY = "MODEL_NAME"
+IS_STEMMED_ENV_KEY = "IS_STEMMED"
+DEFAULT_MODEL_NAME = "wiki-50"
+
+model_cache = ModelCache()
+
+
+def set_language_data_folder(language_data_folder: str):
+    model_cache.language_data_folder = language_data_folder
+
+
+def is_loaded(model_identifier: ModelIdentifier) -> bool:
+    return model_cache.is_loaded(model_identifier)
+
+
+def load_model(model_identifier: ModelIdentifier) -> KeyedVectors:
+    return model_cache.load_model(model_identifier)
+
+
+def load_model_async(model_identifier: ModelIdentifier):
+    thread = Thread(target=load_model, args=(model_identifier,))
+    thread.start()
+
+
+def load_language(language: str, model_name: Optional[str] = None, is_stemmed: Optional[bool] = None) -> KeyedVectors:
+    if model_name is None:
+        model_name = os.environ.get(key=MODEL_NAME_ENV_KEY, default=DEFAULT_MODEL_NAME)  # type: ignore
+    if is_stemmed is None:
+        is_stemmed = bool(os.environ.get(key=IS_STEMMED_ENV_KEY, default=False))  # type: ignore
+    model_identifier = ModelIdentifier(language=language, model_name=model_name, is_stemmed=is_stemmed)
+    return load_model(model_identifier)
+
+
+def load_language_async(language: str, model_name: Optional[str] = None, is_stemmed: Optional[bool] = None):
+    thread = Thread(target=load_language, args=(language, model_name, is_stemmed), daemon=True)
+    thread.start()
+
+
+# def load_word2vec_format(language_base_folder: str, model_name: str) -> KeyedVectors:
+#     # Where data structure is `*.bin`
+#     file_path = os.path.join(language_base_folder, f"{model_name}.bin")
+#     data = KeyedVectors.load_word2vec_format(file_path, binary=True)
+#     return data
