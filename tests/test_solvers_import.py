02d64401d8acaa98f94a34fcecd521eb4e050813
Asaf Kali <akali93@gmail.com>
2024-01-05 00:39:15 +0200

    🏃 Minor refactor.

--- tests/test_solvers_import.py
+++ tests/test_solvers_import.py
@@ -2,7 +2,6 @@ def test_all_solvers_can_be_imported():
     from solvers.cli import CliGuesser, CliHinter  # noqa
     from solvers.gpt import GPTGuesser, GPTHinter, GPTPlayer  # noqa
     from solvers.naive import (  # noqa
-        ModelAwareCliGuesser,
         NaiveGuesser,
         NaiveHinter,
         NaivePlayer,
