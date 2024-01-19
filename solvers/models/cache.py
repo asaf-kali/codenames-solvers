8e9a13fb1116786dbf1fc41f4bc86713d05ed360
Asaf Kali <akali93@gmail.com>
2024-01-19 14:46:01 +0200

    🎉 Language models: try load from Gensim API if path KV load fails.

--- solvers/models/cache.py
+++ solvers/models/cache.py
@@ -4,6 +4,7 @@ from os.path import expanduser
 from threading import Lock
 from typing import Dict
 
+import gensim.downloader as gensim_api
 from generic_iterative_stemmer.models import StemmedKeyedVectors
 from gensim.models import KeyedVectors
 
@@ -13,8 +14,8 @@ log = logging.getLogger(__name__)
 
 
 class ModelCache:
-    def __init__(self):
-        self.language_data_folder = "~/.cache/language_data"
+    def __init__(self, language_data_folder: str = "~/.cache/language_data"):
+        self.language_data_folder = language_data_folder
         self._cache: Dict[ModelIdentifier, KeyedVectors] = {}
         self._main_lock = Lock()
         self._model_locks: Dict[ModelIdentifier, Lock] = {}
@@ -35,25 +36,33 @@ class ModelCache:
             return self._cache[model_identifier]
 
     def _load_model(self, model_identifier: ModelIdentifier) -> KeyedVectors:
-        # TODO: in case loading fails, try gensim downloader
-        # import gensim.downloader as api
-        # model = api.load("wiki-he")
         log.info("Loading model...", extra={"model": model_identifier.dict()})
         language_base_folder = expanduser(os.path.join(self.language_data_folder, model_identifier.language))
-        model = load_kv_format(
-            language_base_folder=language_base_folder,
-            model_name=model_identifier.model_name,
-            is_stemmed=model_identifier.is_stemmed,
-        )
-        log.info("Model loaded", extra={"model": model_identifier.dict()})
-        return model
+        try:
+            return load_kv_format(
+                language_base_folder=language_base_folder,
+                model_name=model_identifier.model_name,
+                is_stemmed=model_identifier.is_stemmed,
+            )
+        except Exception as e:
+            log.warning(f"Failed to load model: {e}", exc_info=True)
+            return load_from_gensim(model_identifier)
 
 
 def load_kv_format(language_base_folder: str, model_name: str, is_stemmed: bool = False) -> KeyedVectors:
     model_folder = os.path.join(language_base_folder, model_name)
-    file_path = os.path.join(model_folder, "model.kv")  # TODO: This needs fixing
+    file_path = os.path.join(model_folder, "model.kv")
+    log.debug(f"Looking for [{model_name}] in {file_path}...")
     if is_stemmed:
         model = StemmedKeyedVectors.load(file_path)
     else:
         model = KeyedVectors.load(file_path)
+    log.debug(f"Successfully loaded [{model_name}] from {file_path}")
+    return model
+
+
+def load_from_gensim(model_identifier: ModelIdentifier) -> KeyedVectors:
+    log.debug(f"Looking for [{model_identifier.model_name}] in gensim API...")
+    model = gensim_api.load(model_identifier.model_name)
+    log.debug(f"Successfully loaded [{model_identifier.model_name}] from gensim API")
     return model
